47c96b61946b7f3810459f1e1302346eb515ba44
Rando Rostok <rrostok@malwarebytes.com>
2021-08-04 23:57:20 +0300

    converted to tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App(): React.ReactElement {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+        <div className="text-center">
+            <header className="bg-gray-500 min-h-screen flex flex-col items-center justify-center text-base text-white">
+                <img src={logo} className="animate-spin-slow h-14 pointer-events-none" alt="logo" />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                <a className="text-light-blue" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
             </header>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        textColor: (theme) => {
+            const col = theme('colors');
+            col['light-blue'] = '#61dafb';
+            return col;
+        },
+        extend: {
+            animation: {
+                'spin-slow': 'spin 20s linear infinite',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
